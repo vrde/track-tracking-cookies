f1de566502ddc9c9db1de80600214602898efb39
vrde <agranzot@gmail.com>
2018-04-30 01:53:53 +0200

    Add UI to see all your cookies

---
+++ README.md
@@ -0,0 +1,2 @@
+# Install
+Checkout the code and load the extension as "unpacked" ([Chrome](https://developer.chrome.com/extensions/getstarted), [Firefox](https://developer.mozilla.org/en-US/Add-ons/WebExtensions/Temporary_Installation_in_Firefox)).
---
+++ js/home.js
@@ -0,0 +1,51 @@
+function t_group(group) {
+	var cookies = ''
+
+	for (var i = 0; i < group.cookies.length; i++) {
+		cookies += `<li>${t_cookie(group.cookies[i])}</li>`
+	}
+
+	return `<div>
+			<h2>${e(group.domain)}</h2>
+			<ul>${cookies}</ul>
+		</div>`
+}
+
+function t_cookie(cookie) {
+	var expiration = new Date(parseInt(cookie.expirationDate, 10) * 1000);
+	return `${e(cookie.name)}: ${e(cookie.value)} (exp: ${expiration.toString()})`
+}
+
+function groupCookies(cookies) {
+	const groups = {}
+	for (var i = 0; i < cookies.length; i++) {
+		const cookie = cookies[i]
+		const domain = cookie.domain
+
+		if (!groups[domain]) {
+			groups[domain] = {
+				domain: domain,
+				cookies: []
+			}
+		}
+
+		groups[domain].cookies.push(cookie)
+	}
+	return groups
+}
+
+chrome.cookies.getAll({}, cookies => {
+	const groups = groupCookies(cookies)
+	console.log(groups)
+	const main = document.getElementById('main')
+	const ul = document.createElement('ul')
+	main.appendChild(ul)
+
+	Object.keys(groups).forEach(function(domain) {
+		const group = groups[domain]
+		const li = document.createElement('li')
+		li.innerHTML = t_group(group)
+		ul.appendChild(li)
+	})
+});
+
